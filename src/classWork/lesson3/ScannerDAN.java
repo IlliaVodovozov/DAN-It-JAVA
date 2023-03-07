ac8d4d15ac82ab539e06f5853cbc6586dfda1a75
illia <vodovozov.illia@gmail.com>
2023-03-07 19:52:09 +0200

    lesson3 -scanner

---
+++ src/classWork/lesson3/ScannerDAN.java
@@ -0,0 +1,18 @@
+package classWork.lesson3;
+
+import java.util.Scanner;
+
+public class ScannerDAN {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+  /*      System.out.println("Введите строку");*/
+        /*String string = input.nextLine();
+        System.out.println(string);*/
+        String string2 = input.next();
+        System.out.println(string2);
+        String string3 = input.next();
+        System.out.println(string3);
+        int x = input.nextInt();
+        System.out.println(x);
+    }
+}
